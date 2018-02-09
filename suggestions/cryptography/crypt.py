cf0ec85e9470d108e834eaebc4e56ec016e48e20
Gronnesby <morten.gronnesby@uit.no>
2018-02-09 15:00:32 +0100

    Allowed for letters to be part of the key.

--- suggestions/cryptography/crypt.py
+++ suggestions/cryptography/crypt.py
@@ -1,9 +1,28 @@
 import string
 
 
-def caesar_cipher_encrypt():
-    encryption_key = int(raw_input("Enter encryption key: ")) % 256
+def get_message():
+
     message = str(raw_input("Enter message: "))
+    print "\n"
+    return message
+
+
+def get_key():
+
+    encryption_key = 0
+    key = raw_input("Enter encryption key: ")
+    for letter in key:
+        if letter.isdigit():
+            encryption_key += int(letter)
+        else:
+            encryption_key += ord(letter)
+
+    return (encryption_key % 256)
+
+def caesar_cipher_encrypt(message, encryption_key):
+
+
 
     encrypted = ""
 
@@ -11,8 +30,7 @@ def caesar_cipher_encrypt():
         print "Encrypting {0}: {1} + {2} % 256 = {3}".format(letter, ord(letter), encryption_key, ((ord(letter) + encryption_key) % 256))
         encrypted += chr((ord(letter) + encryption_key) % 256)
 
-    print "Cipher text: {0}\n".format(repr(str(encrypted)))
-    return encrypted, encryption_key
+    return encrypted
 
 
 def caesar_cipher_decrypt(literal, encryption_key):
@@ -20,10 +38,18 @@ def caesar_cipher_decrypt(literal, encryption_key):
     decrypted = ""
     for letter in literal:
         decrypted += chr((ord(letter) - encryption_key) % 256)
-        print "Decrypting {0}: {1} - {2} % 256 = {3}".format(letter, ord(letter), encryption_key, ((ord(letter) - encryption_key) % 256))
+        print "Decrypting {0}: {1} - {2} % 256 = {3}".format(repr(letter), ord(letter), encryption_key, ((ord(letter) - encryption_key) % 256))
 
-    print "\nDecrypted Message: {0}".format(decrypted)
+    return decrypted
+
 
 if __name__ == "__main__":
-    cipher_text, key = caesar_cipher_encrypt()
-    caesar_cipher_decrypt(cipher_text, key)
+
+    key = get_key()
+    message = get_message()
+
+    cipher_text = caesar_cipher_encrypt(message, key)
+    print "\nCipher text: {0}\n".format(repr(str(cipher_text)))
+
+    decrypted_text = caesar_cipher_decrypt(cipher_text, key)
+    print "\nDecrypted Message: {0}".format(decrypted_text)
