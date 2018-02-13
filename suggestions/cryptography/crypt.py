f86b1004355412964890109d4e9672d76848f89f
Morten Grønnesby <Gronnesby@users.noreply.github.com>
2018-02-14 00:00:27 +0100

    Commented code, and added the possibility to use letters as keys

--- suggestions/cryptography/crypt.py
+++ suggestions/cryptography/crypt.py
@@ -2,46 +2,40 @@ import string
 
 
 def get_message():
-
+    
+    # Get a message to encrypt
     message = str(raw_input("Enter message: "))
-    print "\n"
     return message
 
 
 def get_key():
 
+    # Get an encryption key
     encryption_key = 0
     key = raw_input("Enter encryption key: ")
+    
+    # Convert non-digit characters to numbers
     for letter in key:
         if letter.isdigit():
             encryption_key += int(letter)
         else:
+            # If the input contains a letter, use its ascii-value instead
             encryption_key += ord(letter)
 
+    # Limit the key to max 255
     return (encryption_key % 256)
 
 def caesar_cipher_encrypt(message, encryption_key):
-
-
-
+    
     encrypted = ""
-
+    
+    # Encrypt each letter of the message
     for letter in message:
+        # Print the formula for encrypting each letter
         print "Encrypting {0}: {1} + {2} % 256 = {3}".format(letter, ord(letter), encryption_key, ((ord(letter) + encryption_key) % 256))
         encrypted += chr((ord(letter) + encryption_key) % 256)
 
     return encrypted
-
-
-def caesar_cipher_decrypt(literal, encryption_key):
-
-    decrypted = ""
-    for letter in literal:
-        decrypted += chr((ord(letter) - encryption_key) % 256)
-        print "Decrypting {0}: {1} - {2} % 256 = {3}".format(repr(letter), ord(letter), encryption_key, ((ord(letter) - encryption_key) % 256))
-
-    return decrypted
-
 
 if __name__ == "__main__":
 
@@ -50,6 +44,3 @@ if __name__ == "__main__":
 
     cipher_text = caesar_cipher_encrypt(message, key)
     print "\nCipher text: {0}\n".format(repr(str(cipher_text)))
-
-    decrypted_text = caesar_cipher_decrypt(cipher_text, key)
-    print "\nDecrypted Message: {0}".format(decrypted_text)
