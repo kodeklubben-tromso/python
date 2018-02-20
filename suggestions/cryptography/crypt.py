19a4f840fd85f9b1e5f7fb265b4c2cd9a38943f2
Gronnesby <morten.gronnesby@uit.no>
2018-02-20 12:31:13 +0100

    Added password hashing to the cryptography example

--- suggestions/cryptography/crypt.py
+++ suggestions/cryptography/crypt.py
@@ -1,5 +1,6 @@
-import string
-
+import uuid
+import hashlib
+import getpass
 
 def get_message():
 
@@ -20,12 +21,10 @@ def get_key():
 
     return (encryption_key % 256)
 
+
 def caesar_cipher_encrypt(message, encryption_key):
 
-
-
     encrypted = ""
-
     for letter in message:
         print "Encrypting {0}: {1} + {2} % 256 = {3}".format(letter, ord(letter), encryption_key, ((ord(letter) + encryption_key) % 256))
         encrypted += chr((ord(letter) + encryption_key) % 256)
@@ -43,13 +42,38 @@ def caesar_cipher_decrypt(literal, encryption_key):
     return decrypted
 
 
-if __name__ == "__main__":
-
+def run_caesar_cipher():
     key = get_key()
     message = get_message()
 
     cipher_text = caesar_cipher_encrypt(message, key)
-    print "\nCipher text: {0}\n".format(repr(str(cipher_text)))
+    print "\nCipher text: {0}\n".format(cipher_text)
 
     decrypted_text = caesar_cipher_decrypt(cipher_text, key)
     print "\nDecrypted Message: {0}".format(decrypted_text)
+
+
+def hash_password(pw):
+
+    hashobj = hashlib.sha256(pw)
+    return hashobj.hexdigest()
+
+
+def set_password():
+
+    hashed = hash_password(getpass.getpass(prompt="Set Password: "))
+    return hashed
+
+def run_login():
+    
+    password_hash = set_password()
+    pw = getpass.getpass(prompt="Password: ")
+
+    if password_hash != hash_password(pw):
+        print "Access denied"
+    else:
+        run_caesar_cipher()
+
+if __name__ == "__main__":
+
+    run_login()
